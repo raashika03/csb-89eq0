e0c25afd9a70dfdf103b5d85d2f10dc3ec6f8878
Code Sandbox <hello@codesandbox.io>
2021-05-24 01:34:01 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.89d6dccea71ee8f886c2875da338ec93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82a10f110307f2b06f16",
+    "url": "https://raashika03.github.io/csb-89eq0/static/css/main.c60726c7.chunk.css"
+  },
+  {
+    "revision": "82a10f110307f2b06f16",
+    "url": "https://raashika03.github.io/csb-89eq0/static/js/main.8570e0fc.chunk.js"
+  },
+  {
+    "revision": "fac07dc537647890c5bb",
+    "url": "https://raashika03.github.io/csb-89eq0/static/js/runtime~main.9a9e962b.js"
+  },
+  {
+    "revision": "9aab5f06b034967befb6",
+    "url": "https://raashika03.github.io/csb-89eq0/static/js/2.10f4b8ce.chunk.js"
+  },
+  {
+    "revision": "8797ab82c2561408a53231499f6258f8",
+    "url": "https://raashika03.github.io/csb-89eq0/index.html"
+  }
+];
